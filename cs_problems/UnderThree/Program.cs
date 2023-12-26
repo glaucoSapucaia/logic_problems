16523b0a0be39265a3f9bf24d9dc21f289acad54
glaucoSapucaia <glaucopco@gmail.com>
2023-12-26 18:04:14 -0300

    C# under three

---
+++ cs_problems/UnderThree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+// culture
+CultureInfo CI = CultureInfo.InvariantCulture;
+
+// variables
+int a, b, c, lower_number;
+
+// inputs
+Console.WriteLine("Enter three integers:");
+Console.Write("A = ");
+a = int.Parse(Console.ReadLine());
+Console.Write("B = ");
+b = int.Parse(Console.ReadLine());
+Console.Write("C = ");
+c = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+// logic
+if (a < b && a < c) {
+    lower_number = a;
+}
+else if (b < c) {
+    lower_number = b;
+}
+else {
+    lower_number = c;
+}
+
+// answer
+Console.WriteLine("Lower number -> " + lower_number);
